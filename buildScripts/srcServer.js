a4f1064ffeec8acb2fb86b785413bb839cf82f7a
Amber <adm2037@gmail.com>
2017-12-27 22:21:54 -0600

    express comment

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -5,6 +5,7 @@ var open = require('open');
 var port = 3000;
 var app = express();
 
+//when run with node command in CLI express is coming in, sees that we are requesting the root and then sending the file we specified here and opening the app at the local host 3000
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 })
