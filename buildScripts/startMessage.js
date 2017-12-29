854ac35778808d1be84ddc11f9a59590383158fa
Amber <adm2037@gmail.com>
2017-12-28 19:51:47 -0600

    prestart script

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,6 @@
+//chalk allows us to specify the color of the output we are displaying to the console.
+var chalk = require('chalk');
+
+console.log(chalk.green('Starting app in dev mode...'));
+
+//added prestart to json scripts
