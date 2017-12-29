edd097c1cbeb6d08049a19ad6198ba9b1410eef7
Amber <adm2037@gmail.com>
2017-12-28 21:55:39 -0600

    babel -- npm start

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,5 +1,8 @@
 //chalk allows us to specify the color of the output we are displaying to the console.
-var chalk = require('chalk');
+//var chalk = require('chalk'); the common js pattern used for node
+
+//use module syntax [es6] node doesnt understand this so use babel node -- change prestart command in json
+import chalk from 'chalk';
 
 console.log(chalk.green('Starting app in dev mode...'));
 
